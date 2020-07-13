2ec0b1190f063f04cc706d675bf365865e1b1479
Victor Salvaya <victorsalvaya@hotmail.com>
2020-07-13 16:17:21 -0300

    alterando para o modelo da mayara

--- script/main.js
+++ script/main.js
@@ -32,17 +32,17 @@ const animationTimeline = () => {
     .to(".container", 0.1, {
       visibility: "visible"
     })
-    .from(".one", 0.7, {
+    .from(".one", 1, {
       opacity: 0,
       y: 10
     })
-    .from(".two", 0.4, {
+    .from(".two", 1, {
       opacity: 0,
       y: 10
     })
     .to(
       ".one",
-      0.7,
+      1.0,
       {
         opacity: 0,
         y: 10
@@ -51,7 +51,7 @@ const animationTimeline = () => {
     )
     .to(
       ".two",
-      0.7,
+      1,
       {
         opacity: 0,
         y: 10
@@ -101,18 +101,18 @@ const animationTimeline = () => {
       },
       "+=0.7"
     )
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
+    .from(".idea-1", 1.0, ideaTextTrans)
+    .to(".idea-1", 1.0, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 1.0, ideaTextTrans)
+    .to(".idea-2", 1.0, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 2.0, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-3", 2.0, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
